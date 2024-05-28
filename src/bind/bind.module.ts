3a291cd281d370d2d8ffcc24674b246a06ed8579
monasteriosm <miguel421909@gmail.com>
2024-05-28 09:01:06 -0400

    integracion de temporal

--- src/bind/bind.module.ts
+++ src/bind/bind.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BindService } from './bind.service';
+import { TempLogModule } from 'src/temp-log/temp-log.module';
 
 @Module({
   imports:[
+    TempLogModule
   ],
   providers: [BindService],
   exports: [BindService],
